0553fb1fac3bdcb42f2124b18f97105552202712
zhaoguochun1995 <zhaoguochun1995@163.com>
2022-11-28 19:08:10 +0800

    fix no need change and modify yolov5/yolov5_s-v61_syncbn_8xb16-300e_coco batchsize to 8

--- configs/yolov5/yolov5_l-v61_syncbn_fast_8xb16-300e_coco.py
+++ configs/yolov5/yolov5_l-v61_syncbn_fast_8xb16-300e_coco.py
@@ -3,6 +3,17 @@ _base_ = './yolov5_m-v61_syncbn_fast_8xb16-300e_coco.py'
 deepen_factor = 1.0
 widen_factor = 1.0
 
+train_batch_size_per_gpu=8
+
+optim_wrapper = dict(
+    optimizer=dict(
+        batch_size_per_gpu=train_batch_size_per_gpu),
+    )
+
+train_dataloader = dict(
+    batch_size=train_batch_size_per_gpu
+    )
+
 model = dict(
     backbone=dict(
         deepen_factor=deepen_factor,
